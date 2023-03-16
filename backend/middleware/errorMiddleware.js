d660bea41b43518f3abc8a8754036b721e3faf0d
Philling1 <mbadughaphilip5@gmail.com>
2023-03-16 10:04:54 +0100

    fixing the server.js file that causes error on deploy

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,7 +1,6 @@
 const errorHandler = (error, _, res, next) => {
    //FIX: check for bad status codes, if it's a good status code then we want to send a bad status code 
    const statusCode = res.statusCode <400 ? 500 : res.statusCode
-   console.log('error middleware')
 
    res.status(statusCode)
    res.json({
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 //Connecting to database
 connectDB()
@@ -21,16 +21,12 @@ app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // Serve Frontend
 if (process.env.NODE_ENV === 'production') {
-  // Set build folder as static
+  // Set build folder as static 
   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   // FIX: below code fixes app crashing on refresh in deployment
   app.get('*', (_, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-  })
-} else {
-  app.get('/', (_, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
   })
 }
 
